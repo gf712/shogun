9a69eb173b5bc620febb3361c34db89199dcf6e3
gf712 <gil_f.hoben@hotmail.com>
2020-06-12 13:23:13 +0100

    cleanup files

--- examples/undocumented/python/kernel_poly.py
+++ examples/undocumented/python/kernel_poly.py
@@ -5,11 +5,10 @@ testdat = '../data/fm_test_real.dat'
 parameter_list = [[traindat,testdat,4,0.0],[traindat,testdat,5,0.0]]
 
 def kernel_poly (train_fname=traindat,test_fname=testdat,degree=4,c=0.0):
-	from shogun import CSVFile
 	import shogun as sg
 
-	feats_train=sg.create_features(CSVFile(train_fname))
-	feats_test=sg.create_features(CSVFile(test_fname))
+	feats_train=sg.create_features(sg.read_csv(train_fname))
+	feats_test=sg.create_features(sg.read_csv(test_fname))
 
 	kernel = sg.create_kernel("PolyKernel", degree=degree, c=c)
 	kernel.init(feats_train, feats_train)
